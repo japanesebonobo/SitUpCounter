8d324af0c7bc018017dec2bc865fdf70ec0c4867
japanesebonobo <tomi119119@gmail.com>
2026-03-26 00:44:50 +0900

    参照と更新のエンドポイント実装

--- src/main/java/yoshitomi/apps/situpcounter/service/CounterService.java
+++ src/main/java/yoshitomi/apps/situpcounter/service/CounterService.java
@@ -1,11 +1,22 @@
 package yoshitomi.apps.situpcounter.service;
 
 import org.springframework.stereotype.Service;
+import yoshitomi.apps.situpcounter.repository.CounterDataRepository;
 
 @Service
 public class CounterService {
 
+    private final CounterDataRepository counterDataRepository;
+
+    public CounterService(CounterDataRepository counterDataRepository) {
+        this.counterDataRepository = counterDataRepository;
+    }
+
     public String getCounterValue() {
-        return "1";
+        return String.valueOf(counterDataRepository.getCounter());
+    }
+
+    public void updateCounterValue() {
+        counterDataRepository.updateCounter();
     }
 }
